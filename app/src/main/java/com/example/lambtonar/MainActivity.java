4986c773a696f79bd0a8c9f070cf70055bb8e734
himanshu <hmehta095@gmail.com>
2019-12-06 18:31:46 -0500

    texture,media played and arFragment in MainActivity defined

--- app/src/main/java/com/example/lambtonar/MainActivity.java
+++ app/src/main/java/com/example/lambtonar/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.lambtonar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.rendering.ExternalTexture;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ExternalTexture texture;
+    private MediaPlayer mediaPlayer;
+    private customAR arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
