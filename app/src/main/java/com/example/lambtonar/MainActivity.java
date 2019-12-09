a8878bd19ec05bb7831ae7e02c14c1eefa4202fa
himanshu <hmehta095@gmail.com>
2019-12-08 22:31:46 -0500

    Render model created

--- app/src/main/java/com/example/lambtonar/MainActivity.java
+++ app/src/main/java/com/example/lambtonar/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.lambtonar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.ExternalTexture;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 
 public class MainActivity extends AppCompatActivity {
 
     private ExternalTexture texture;
     private MediaPlayer mediaPlayer;
     private customAR arFragment;
+    private Scene scene;
+    private ModelRenderable renderable;
+    private boolean isImageDetected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        texture = new ExternalTexture();
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.video);
+        mediaPlayer.setSurface(texture.getSurface());
+        mediaPlayer.setLooping(true);
+
+        ModelRenderable
+                .builder()
+                .setSource(this, Uri.parse("video_screen.sfb"))
+                .build()
+                .thenAccept(modelRenderable -> {
+                    modelRenderable.getMaterial().setExternalTexture("videoTexture",texture);
+                    modelRenderable.getMaterial().setFloat4("keyColor",new Color(1f,1f,1f));
+                    renderable = modelRenderable;
+                });
+
+
+        arFragment = (customAR) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        scene = arFragment.getArSceneView().getScene();
+
+        scene.addOnUpdateListener(this::onUpdate);
+
 
     }
+
+    private void onUpdate(FrameTime frameTime) {
+    }
+
+
 }
